06665120e258d498bd8f2dacc68f605dc5274b5b
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-17 22:28:18 -0500

    Create user.js

---
+++ util/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+  userId: String,
+  coins: { type: Number, default: 0 },
+  blacklisted: { type: Boolean, default: false }
+})
+
+module.exports = mongoose.model("User", schema)
