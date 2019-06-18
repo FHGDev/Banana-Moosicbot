557c273c88ab192b8ec0a2fa6f8a17b61c70c4f1
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-18 10:52:34 -0500

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args) => {
+
+}
+
+module.exports.help = {
+  name: "play",
+  usage: "b.play songname or https://youtube.com/watch?v=some-video-id",
+  info: "Plays a song in your voice channel."
+}
