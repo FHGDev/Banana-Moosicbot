83fdaa5302078d934e9bfd00d5eab634c3f54d23
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-18 11:38:56 -0500

    Update user.js

--- util/models/user.js
+++ util/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema({
   userId: String,
   coins: { type: Number, default: 0 },
-  blacklisted: { type: Boolean, default: false }
+  blacklisted: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false }
 })
 
 module.exports = mongoose.model("User", schema)
