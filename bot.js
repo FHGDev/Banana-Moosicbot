e6eff9c0930f7d360921af825e324b9c7ae175d3
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-17 22:20:03 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,55 @@
+console.clear()
+console.log("Loading Banana Moosicbot...")
+
+const discord = require('discord.js')
+const mongoose = require('mongoose')
+const bot = new discord.Client()
+
+bot.commands = new discord.Collection()
+bot.player = new (require('discord.js-musicplayer').Player)(bot)
+bot.prefix = "b."
+bot.owner;
+bot.invite;
+global.embed = discord.RichEmbed
+
+setTimeout(() => {
+  mongoose.connect(`mongodb+srv://FHGDev:${process.env.atlaspass}@cluster0-ra6vb.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongoose connected.");
+  })
+  .catch(err => console.error(err))
+}, 2500)
+
+require('fs').readdir("./commands/", (err, files) => {
+  files.filter(f => f.split(".").pop() === "js").forEach((f,i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`))
+  })
+})
+
+bot.on('ready', () => {
+  console.log("Banana Moosicbot is ready for action!")
+  bot.user.setActivity(`for ${bot.prefix}help | ${bot.guilds.size} servers`, {type: "WATCHING"})
+  
+  bot.fetchApplication("@me")
+  .then(app => bot.owner = app.owner)
+  
+  bot.generateInvite(["ADMINISTRATOR"])
+  .then(inv => bot.invite = inv)
+})
+
+bot.on('message', message => {
+  if (message.content.startsWith(bot.prefix)) return;
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  
+  const mArray = message.content.split(" ")
+  const args = mArray.slice(1)
+  const cmd = bot.commands.get(mArray[0].slice(bot.prefix.length))
+  
+  if (cmd) {
+    cmd.run(bot, message, args)
+    console.log(`${message.author.username} used the ${cmd.help.name} command.`)
+  }
+})
+
+bot.login(process.env.token)
